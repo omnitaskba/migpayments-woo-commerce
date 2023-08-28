88821503ffcccecf9f848c64836e9a168f8d898a
Admir <admir@omnitask.ba>
2023-08-28 22:23:22 +0200

    version

--- migpayments.php
+++ migpayments.php
@@ -3,7 +3,7 @@
 Plugin Name: 		MigPayments WooCommerce
 Plugin URI: 		https://migpayments.tech
 Description: 		A crypto payment gateway
-Version: 			1.6.1
+Version: 			1.6.2
 Author: 			Omnitask
 Author URI: 		https://migpayments.tech
 */
@@ -38,7 +38,7 @@ if (!function_exists('migpayments_wc_gateway_load') && !function_exists('migpaym
 	
 	// 
 	DEFINE('MIGPAYMENTSWC', 'migpayments-woocommerce');
-	DEFINE('MIGPAYMENTSWC_VERSION', '1.6.1');
+	DEFINE('MIGPAYMENTSWC_VERSION', '1.6.2');
 
 	if (!defined('MIGPAYMENTSWC_AFFILIATE_KEY')){
 		
